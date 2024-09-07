7d7976d649bf73c44f3922580793694959d952f4
HafizOwais2003 <hafizowais2003@gmail.com>
2024-09-07 15:07:33 -0700

    Update

---
+++ first-jupyter-notebook/main.py
@@ -0,0 +1,19 @@
+name:str = "OWAIS Arshad"
+
+#object.method()
+#object.atribute
+
+print( (name.capitalize() ) )
+print( (name.casefold() ) )
+print( (name.center(25, "*") ) )
+print( (name.upper() ) )
+print( (name.lower() ) )
+print( (name.title() ) )
+
+
+
+
+
+
+
+
